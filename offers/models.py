19c424d7490b445abd683a1eeb49fa4f557ced42
Kostiantyn Ivashchenko <civaschenko@yahoo.com>
2023-11-11 21:38:11 +0200

    updated models etc

--- offers/models.py
+++ offers/models.py
@@ -17,23 +17,6 @@ class Category(models.Model):
         return f"{self.category_name} ({self.name_ua})"
 
 
-class AddressDelivery(models.Model):
-    # TODO: Add Celery task to create address from coordinates and apply to certain offer record
-    address = models.TextField(max_length=500, null=True)
-    country = models.CharField(max_length=255, null=True)
-    geometry_point = models.PointField(verbose_name="Location", srid=4326)
-
-    def __repr__(self):
-        return f"<AddressDelivery(id={self.id} address={self.address} lat={self.lat} lng={self.lng} geometry_point={self.geometry_point})>"
-
-    def __str__(self):
-        return f"{self.address}"
-
-    class Meta:
-        db_table = "addresses"
-        verbose_name_plural = "Addresses Delivery"
-
-
 class ActualOffers(models.Manager):
     def get_queryset(self):
         return (
@@ -45,7 +28,7 @@ class ActualOffers(models.Manager):
 
 
 class Offer(models.Model):
-    # author = models.ForeignKey(User, on_delete=models.PROTECT, related_name="offers")
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name="offers")
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name="offers"
     )
@@ -55,16 +38,17 @@ class Offer(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
     currency = models.CharField(max_length=3)
     amount = models.DecimalField(max_digits=8, decimal_places=2, null=True)
-    terms_delivery = models.CharField(
-        max_length=3, help_text="for example, FCA", null=True
-    )
+    terms_delivery = models.CharField(max_length=3, help_text="for example, FCA", null=True)
     created_at = models.DateField(auto_now=True)
     expired_at = models.DateField(null=True)
     is_active = models.BooleanField(default=True)
     latitude = models.DecimalField(max_digits=18, decimal_places=16, null=True)
     longitude = models.DecimalField(max_digits=18, decimal_places=16, null=True)
     details = models.CharField(max_length=255, null=True)
-    address = models.ForeignKey("AddressDelivery", on_delete=models.SET_NULL, null=True)
+    # geometry location
+    geometry_point = models.PointField(verbose_name="Location", srid=4326)
+    address = models.TextField(max_length=500, null=True)
+    country = models.CharField(max_length=255, null=True)
 
     objects = models.Manager()
     actual = ActualOffers()  # offers where expired datetime less or equal now
