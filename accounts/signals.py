0ff72ac9ecd23307e28398f37abcf72c7a9043a8
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-01-11 16:26:53 +0200

    added accounts tests auto create Profile

--- accounts/signals.py
+++ accounts/signals.py
@@ -7,12 +7,12 @@ from accounts.models import Profile
 user_model = get_user_model()
 
 
-@receiver(post_save, sender=user_model)
+@receiver(post_save, sender=user_model, dispatch_uid="auto_create_profile")
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=user_model)
+@receiver(post_save, sender=user_model, dispatch_uid="auto_save_profile")
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from unittest.mock import patch
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from accounts.models import Profile
+
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        client = Client()
+
+    # @patch("django.db.models.signals.ModelSignal.send")
+    def test_auto_create_profile_after_registration_successful(self):
+        form_data = dict(
+            username='test-user-1',
+            email='test-user-1@graintrade.info',
+            password1='6gB7^)v4;XM#-X!',
+            password2='6gB7^)v4;XM#-X!',
+        )
+        # register
+        r = self.client.post('/accounts/signup/', form_data)
+        self.assertEqual(r.status_code, 302)
+        user = User.objects.last()
+        self.assertEqual(user.username, 'test-user-1')
+        self.assertEqual(user.email, 'test-user-1@graintrade.info')
+        p = Profile.objects.last()
+        self.assertEqual(p.user.username, 'test-user-1')
+        self.assertEqual(p.full_name, 'Full name')
+        self.assertEqual(p.company, 'Company name')
